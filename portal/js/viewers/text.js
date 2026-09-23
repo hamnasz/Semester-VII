e89c95ea347d166d0e9016c2f8bb708d5c426c8a
Developer Hamna <168326829+hamnasz@users.noreply.github.com>
2026-09-23 05:19:01 +0000

    Removed Frontend

--- portal/js/viewers/text.js
+++
@@ -1,6 +0,0 @@
-import { el } from '../core/dom.js';
-
-export async function render(node, ctx) {
-  const text = await (await fetch(ctx.contentUrl)).text();
-  ctx.host.append(el('pre', { class: 'txt-plain' }, text));
-}
